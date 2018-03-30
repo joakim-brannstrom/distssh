793c626bdfabd54435cbd1f6e58664322b749c30
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-03-30 15:46:52 +0200

    Split libc binding to a separate file to improve coherency

--- source/distssh/app.d
+++ source/distssh/app.d
@@ -477,23 +477,6 @@ int cli_measureHosts(const Options opts) nothrow {
     return 0;
 }
 
-/**
-DESCRIPTION
-
-     The getloadavg() function returns the number of processes in the system
-     run queue averaged over various periods of time.  Up to nelem samples are
-     retrieved and assigned to successive elements of loadavg[].  The system
-     imposes a maximum of 3 samples, representing averages over the last 1, 5,
-     and 15 minutes, respectively.
-
-
-DIAGNOSTICS
-
-     If the load average was unobtainable, -1 is returned; otherwise, the num-
-     ber of samples actually retrieved is returned.
- */
-extern (C) int getloadavg(double* loadavg, int nelem);
-
 /** Print the load of localhost.
  *
  * #SPC-measure_local_load
@@ -502,6 +485,7 @@ int cli_localLoad(WriterT)(scope WriterT writer) nothrow {
     import std.ascii : newline;
     import std.conv : to;
     import std.parallelism : totalCPUs;
+    import distssh.libc : getloadavg;
 
     try {
         double[3] loadavg;
---
+++ source/distssh/libc_binding.d
@@ -0,0 +1,25 @@
+/**
+Copyright: Copyright (c) 2018, Joakim Brännström. All rights reserved.
+License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
+Author: Joakim Brännström (joakim.brannstrom@gmx.com)
+
+This module contains libc bindings.
+*/
+module distssh.libc;
+
+/**
+DESCRIPTION
+
+     The getloadavg() function returns the number of processes in the system
+     run queue averaged over various periods of time.  Up to nelem samples are
+     retrieved and assigned to successive elements of loadavg[].  The system
+     imposes a maximum of 3 samples, representing averages over the last 1, 5,
+     and 15 minutes, respectively.
+
+
+DIAGNOSTICS
+
+     If the load average was unobtainable, -1 is returned; otherwise, the num-
+     ber of samples actually retrieved is returned.
+ */
+extern (C) int getloadavg(double* loadavg, int nelem);
