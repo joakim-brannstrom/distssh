c910617b1059532731637fa1b3f087248779ee40
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-04-11 16:32:07 +0200

    Add the from idiom template

---
+++ common/source/distssh/from.d
@@ -0,0 +1,11 @@
+/**
+Copyright: Copyright (c) 2018, Joakim Brännström. All rights reserved.
+License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
+Author: Joakim Brännström (joakim.brannstrom@gmx.com)
+*/
+module distssh.from;
+
+/// Local imports everywhere.
+template from(string moduleName) {
+    mixin("import from = " ~ moduleName ~ ";");
+}
