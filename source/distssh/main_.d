30a07f6a2ea2a2dd1883b6b12324cc964a5496c4
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2019-09-29 00:13:14 +0200

    Move types to a separate module

--- source/distssh/main_.d
+++ source/distssh/main_.d
@@ -17,6 +17,7 @@ import logger = std.experimental.logger;
 
 import from_;
 import distssh.process : Pid;
+import distssh.types;
 
 static import std.getopt;
 
@@ -1013,62 +1014,6 @@ void printHelp(const Options opts) nothrow {
     }
 }
 
-struct Host {
-    string payload;
-    alias payload this;
-}
-
-/// The load of a host.
-struct Load {
-    double loadAvg;
-    Duration accessTime;
-    bool unknown;
-
-    bool opEquals(const typeof(this) o) nothrow @safe pure @nogc {
-        if (unknown && o.unknown)
-            return true;
-        return loadAvg == o.loadAvg && accessTime == o.accessTime;
-    }
-
-    int opCmp(const typeof(this) o) pure @safe @nogc nothrow {
-        if (unknown && o.unknown)
-            return 0;
-        else if (unknown)
-            return 1;
-        else if (o.unknown)
-            return -1;
-
-        if (loadAvg < o.loadAvg)
-            return -1;
-        else if (loadAvg > o.loadAvg)
-            return 1;
-
-        if (accessTime < o.accessTime)
-            return -1;
-        else if (accessTime > o.accessTime)
-            return 1;
-
-        return this == o ? 0 : 1;
-    }
-}
-
-@("shall sort the loads")
-unittest {
-    import std.algorithm : sort;
-    import std.array : array;
-    import core.time : dur;
-
-    {
-        auto raw = [Load(0.6, 500.dur!"msecs"), Load(0.5, 500.dur!"msecs")].sort.array;
-        assert(raw[0].loadAvg == 0.5);
-    }
-
-    {
-        auto raw = [Load(0.5, 600.dur!"msecs"), Load(0.5, 500.dur!"msecs")].sort.array;
-        assert(raw[0].accessTime == 500.dur!"msecs");
-    }
-}
-
 /** Login on host and measure its load.
  *
  * Params:
@@ -1326,9 +1271,6 @@ Host[] hostsFromEnv() nothrow {
  */
 struct RemoteHostCache {
     import std.array : array;
-    import std.typecons : Tuple, tuple;
-
-    alias HostLoad = Tuple!(Host, Load);
 
     Duration timeout;
     Host[] remoteHosts;
---
+++ source/distssh/types.d
@@ -0,0 +1,68 @@
+/**
+Copyright: Copyright (c) 2019, Joakim Brännström. All rights reserved.
+License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
+Author: Joakim Brännström (joakim.brannstrom@gmx.com)
+*/
+module distssh.types;
+
+public import std.typecons : Tuple, tuple;
+
+alias HostLoad = Tuple!(Host, Load);
+
+struct Host {
+    string payload;
+    alias payload this;
+}
+
+/// The load of a host.
+struct Load {
+    import std.datetime : Duration;
+
+    double loadAvg;
+    Duration accessTime;
+    bool unknown;
+
+    bool opEquals(const typeof(this) o) nothrow @safe pure @nogc {
+        if (unknown && o.unknown)
+            return true;
+        return loadAvg == o.loadAvg && accessTime == o.accessTime;
+    }
+
+    int opCmp(const typeof(this) o) pure @safe @nogc nothrow {
+        if (unknown && o.unknown)
+            return 0;
+        else if (unknown)
+            return 1;
+        else if (o.unknown)
+            return -1;
+
+        if (loadAvg < o.loadAvg)
+            return -1;
+        else if (loadAvg > o.loadAvg)
+            return 1;
+
+        if (accessTime < o.accessTime)
+            return -1;
+        else if (accessTime > o.accessTime)
+            return 1;
+
+        return this == o ? 0 : 1;
+    }
+}
+
+@("shall sort the loads")
+unittest {
+    import std.algorithm : sort;
+    import std.array : array;
+    import core.time : dur;
+
+    {
+        auto raw = [Load(0.6, 500.dur!"msecs"), Load(0.5, 500.dur!"msecs")].sort.array;
+        assert(raw[0].loadAvg == 0.5);
+    }
+
+    {
+        auto raw = [Load(0.5, 600.dur!"msecs"), Load(0.5, 500.dur!"msecs")].sort.array;
+        assert(raw[0].accessTime == 500.dur!"msecs");
+    }
+}
