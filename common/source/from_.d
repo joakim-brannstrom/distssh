89fe559a84892c5f1a1911155e6190e90384e749
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2019-09-28 19:28:24 +0200

    Rename from module

--- common/source/from_.d
+++ common/source/from_.d
@@ -3,7 +3,7 @@ Copyright: Copyright (c) 2018, Joakim Brännström. All rights reserved.
 License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
 Author: Joakim Brännström (joakim.brannstrom@gmx.com)
 */
-module distssh.from;
+module from_;
 
 /// Local imports everywhere.
 template from(string moduleName) {
